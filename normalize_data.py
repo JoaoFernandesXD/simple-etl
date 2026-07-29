fc6855a9a90fd4f6971ec9a37143f2d5fcff4afa
Joao Fernandes <joaofernandesxd1@icloud.com>
2026-07-29 20:28:06 -0300

    estruturando normalize e documentacao

--- normalize_data.py
+++ normalize_data.py
@@ -1,37 +1,86 @@
 import os
 import pandas as pd 
+import logging
+logging.basicConfig(
+    filename="etl.log",
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(message)s"
+)
 
-input_dir = 'data/bronze' #data raw
-output_dir = 'data/silver'
+
+class NormalizeData:
+    """
+    Executa o processo de normalização dos dados entre as camadas
+    Bronze e Silver da arquitetura Medalhão.
+
+    Responsabilidades:
+        - Ler arquivos CSV e JSON da camada Bronze.
+        - Aplicar transformações e normalizações nos dados.
+        - Remover registros duplicados.
+        - Converter estruturas incompatíveis com Parquet.
+        - Salvar os dados tratados na camada Silver.
+    """
+    def __init__(self, input_dir, output_dir):
+        self.input_dir = input_dir
+        self.output_dir = output_dir
+        os.makedirs(output_dir, exist_ok=True)
+
+    def normalize_data(self) -> None:
+        """
+        Executa o processo de normalização da camada Bronze para a camada Silver.
+
+        Para cada arquivo encontrado no diretório de entrada:
+        - identifica o tipo do arquivo
+        - realiza a leitura dos dados
+        - aplica as transformações necessárias
+        - remove registros duplicados
+        - salva o resultado em formato Parquet no diretório de saída.
+        """
+        for file in os.listdir(self.input_dir):
+            input_path = os.path.join(self.input_dir, file)
+            name, ext = os.path.splitext(file)
+            output_path = os.path.join(self.output_dir, f'{name}.parquet')
+
+            if ext == '.csv':
+                df = pd.read_csv(input_path)
+            elif ext == '.json':
+                try:
+                    df = pd.read_json(input_path)
+                except ValueError:
+                    df = pd.read_json(input_path, lines=True)
+            else:
+                logging.warning(f"Arquivo {file} nao suportado")
+                continue
+    
+            df = self.convert_columns(df)
+           
+            #remove linhas duplicadas
+            df = df.drop_duplicates().reset_index(drop=True)
+        
+            df.to_parquet(output_path, index=False)
+        
+            logging.info(f"Arquivo {file} normalizado e salvo como {output_path}")
+
+    def convert_columns(self, df):
+        """
+            Converte para string os valores do tipo lista presentes nas colunas
+            do DataFrame.
+
+            Essa transformação garante compatibilidade com o formato Parquet,
+            evitando problemas durante a gravação dos dados.
+
+            Args:
+                df (pd.DataFrame): DataFrame a ser normalizado.
+
+            Returns:
+                pd.DataFrame: DataFrame com as colunas tratadas.
+        """
+        for col in df.columns:
+            if df[col].apply(lambda x: isinstance(x, list)).any():
+                df[col] = df[col].apply(lambda x: str(x) if isinstance(x, list) else x)
+        return df
 
 
-os.makedirs(output_dir, exist_ok=True)
-
-
-for file in os.listdir(input_dir):
-    input_path = os.path.join(input_dir, file)
-    name, ext = os.path.splitext(file)
-    output_path = os.path.join(output_dir, f'{name}.parquet')
- 
-    if ext.lower() == '.csv':
-        df = pd.read_csv(input_path)
-    elif ext.lower() == '.json':
-        try:
-            df = pd.read_json(input_path)
-        except ValueError:
-            df = pd.read_json(input_path, lines=True)
-    else:
-        print(f"Arquivo {file} nao suportado")
-        continue
-
-    #garantir que colunas do tipo list converter para string
-    for col in df.columns:
-        if df[col].apply(lambda x: isinstance(x, list)).any():
-            df[col] = df[col].apply(lambda x: str(x) if isinstance(x, list) else x)
-
-    #remove linhas duplicadas
-    df = df.drop_duplicates().reset_index(drop=True)
-
-    df.to_parquet(output_path, index=False)
-
-    print(f"Arquivo {file} normalizado e salvo como {output_path}")
+if __name__ == "__main__":
+    normalize_data = NormalizeData(input_dir = 'data/bronze', output_dir = 'data/silver')
+    normalize_data.normalize_data()
